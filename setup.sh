3c5e24fc4c76e0a434a0f96093a0bf5f305ea204
Karol Zieba <notkarol@gmail.com>
2017-08-01 09:45:51 +0000

    Finally got tensorflow 1.3 compiling

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-function install_opencv()  {
+function install_opencv3()  {
 
     # Install requirements
     sudo apt install -y \
@@ -37,10 +37,10 @@ function install_opencv()  {
 
     # Make sure we have the right branch
     cd opencv
-    git checkout -b 3.3.0-rc
+    git checkout v3.2.0
     cd -
     cd opencv_extra
-    git checkout -b 3.3.0-rc
+    git checkout v3.2.0
     cd -
 
     # Build
@@ -106,6 +106,10 @@ function install_tensorflow() {
 	 swig
 
     if [[ -z $(ls tensorfllow*aarch64.whl) ]] ; then
+
+	# Move cudnn into the right folder
+	sudo mkdir /usr/lib/aarch64-linux-gnu/include/
+	sudo cp /usr/include/cudnn.h /usr/lib/aarch64-linux-gnu/include/cudnn.h
 	
 	# Install Java
 	sudo add-apt-repository ppa:webupd8team/java
@@ -113,15 +117,17 @@ function install_tensorflow() {
 	sudo apt install -y oracle-java8-installer
 
 	# Install bazel
+	wget --no-certificate-check https://github.com/bazelbuild/bazel/releases/download/0.5.2/bazel-0.5.2-dist.zip
 	mkdir bazel
 	cd bazel
-	unzip ../bazel-*-dist.zip
+	unzip ../bazel-0.5.2-dist.zip
 	./compile.sh
 	sudo cp output/bazel /usr/local/bin
 	cd -
+	rm -rf bazel
 
 	# create swap file
-	fallocate -l 5G swapfile
+	fallocate -l 8G swapfile
 	chmod 600 swapfile
 	mkswap swapfile
 	sudo swapon swapfile
@@ -137,32 +143,36 @@ function install_tensorflow() {
 	# Fix numa stuff for ARM
 	sed -i 's/static int TryToReadNumaNode(const string \&pci_bus_id, int device_ordinal) {/static int TryToReadNumaNode(const string \&pci_bus_id, int device_ordinal) {\n  LOG(INFO) << "ARM has no NUMA node, hardcoding to return zero";\n  return 0;/g' tensorflow/stream_executor/cuda/cuda_gpu_executor.cc
 
-	# Move cudnn into the right folder
-	sudo mkdir /usr/lib/aarch64-linux-gnu/include/
-	sudo cp /usr/include/cudnn.h /usr/lib/aarch64-linux-gnu/include/cudnn.h
-
-	# configure. might need to specify a few things here
+	# Replace eigen version
+	sed -i 's/f3a22f35b044/d781c1de9834/g' tensorflow/workspace.bzl
+	sed -i 's/ca7beac153d4059c02c8fc59816c82d54ea47fe58365e8aded4082ded0b820c4/a34b208da6ec18fa8da963369e166e4a368612c14d956dd2f9d7072904675d9b/g' tensorflow/workspace.bzl
+	
+	# configure. default settings except to use python3, cuda, and compute capability to 5.3,6.2
 	./configure
 
 	# Build tensorflow
-	bazel build -c opt --local_resources 3072,4.0,1.0 --verbose_failures --config=cuda \
-	      //tensorflow/tools/pip_package:build_pip_package
+	bazel build --config=opt --config=cuda --local_resources 3072,4.0,1.0 --verbose_failures //tensorflow/tools/pip_package:build_pip_package
 	
 	# Build pip package
-	bazel-bin/tensorflow/tools/pip_package/build_pip_package build_wheel
+	bazel-bin/tensorflow/tools/pip_package/build_pip_package $PWD/build_wheel
 
 	# Move wheel to local directory and install
 	cd ..
-	mv tensorflow/build_wheel/tensorflow*aarch64.whl .
+	mv tensorflow/build_wheel/tensorflow*-cp3*linux_aarch64.whl .
+
+	# remove swap
+	sudo swapoff swapfile
+	sudo rm swapfile
     fi
 
     # Install 
-    sudo pip3 install tensorflow*aarch64.whl
+    sudo pip3 install tensorflow*-cp3*linux_aarch64.whl
 }
 
-
+# Prepare by updating and upgrading all existing packages
 sudo apt update
 sudo apt upgrade -y
 
-install_opencv
+# Install required software
+install_opencv3
 install_tensorflow
