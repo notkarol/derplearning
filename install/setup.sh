73cfcd361b2ce3c4777680032947f3d2ef28be59
Karol Zieba <notkarol@gmail.com>
2017-08-08 01:01:06 +0000

    Updated setup script so it adds usb controller write access to all users

--- install/setup.sh
+++ install/setup.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
+# Make sure we're working off of latest packages
 sudo apt update
 sudo apt upgrade -y
 
+# Make sure we can communicate to Micro Maestro
+pip3 install --user -U pyusb
+sudo apt install libusb-1.0-0-dev
+sudo cp 99-pololu.rules /etc/udev/rules.d/
+sudo udevadm control --reload-rules
+
+# Install necessary software
 ./install_opencv3.sh
 ./install_tensorflow.sh
-./install_pytorch.sh
-./install_ros.sh
+#./install_pytorch.sh
+#./install_ros.sh
