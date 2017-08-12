23ac0fa2ec835bab40cbaa69eb4b7d620ad83388
john <Johnaellis0@gmail.com>
2017-08-12 01:13:03 -0700

    Fixed the printer to not be so god damn sloppy with where it squanches the .pngs

--- train/show_dataset.py
+++ train/show_dataset.py
@@ -17,8 +17,10 @@ def main():
     print(train_x.shape, train_y.shape)
 
     # Store png files
+    if not os.path.exists(train_name):
+        os.makedirs(train_name)
     for i, (x, y) in enumerate(zip(train_x, train_y)):
-        cv2.imwrite("%s_%06i_%.3f_%.3f.png" % (train_name, i, y[0], y[1]), x)
+        cv2.imwrite("%s/%s_%06i_%.3f_%.3f.png" % (train_name, train_name, i, y[0], y[1]), x)
         sys.stdout.write("%.3f\r" % (100.0 * i / len(train_x)))
     print("Done")
 
