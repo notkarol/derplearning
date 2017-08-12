64ca313b0d02ff055285cdeb62926b7c7645a26f
Karol Zieba <notkarol@gmail.com>
2017-08-12 00:56:53 -0700

    Adding h5py requirement

--- install/setup.sh
+++ install/setup.sh
@@ -5,8 +5,8 @@ sudo apt update
 sudo apt upgrade -y
 
 # Make sure we can communicate to Micro Maestro
+sudo apt install libusb-1.0-0-dev python3-h5py
 pip3 install --user -U pyusb
-sudo apt install libusb-1.0-0-dev
 sudo cp 99-pololu.rules /etc/udev/rules.d/
 sudo udevadm control --reload-rules
 
