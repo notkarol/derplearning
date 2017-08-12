020376a85c7e888518b71edda75b840f4e50594a
Karol Zieba <notkarol@gmail.com>
2017-08-12 18:28:13 +0000

    Strong turns

--- drive/driver.py
+++ drive/driver.py
@@ -56,8 +56,8 @@ def main(screen):
                 nn_speed, nn_steer = model.evaluate(frame, speed, steer)
 
                 # dampen
-                servo.move(0.5 * servo.speed + 0.5 * nn_speed)
-                servo.turn(0.5 * servo.steer + 0.5 * nn_steer)
+                servo.move(0.25 * servo.speed + 0.75 * nn_speed)
+                servo.turn(0.25 * servo.steer + 0.75 * nn_steer)
             else:
                 nn_speed, nn_steer = None, None
             log.write((timestamp, speed, nn_speed, steer, nn_steer))
@@ -71,16 +71,16 @@ def main(screen):
         elif c == ord('t'):          recording = False
         elif c == ord('a'):          autonomous = args.model and args.weights
         elif c == ord('s'):          autonomous = False
-        elif c == curses.KEY_LEFT:   servo.turn_left(1E-2)  # large left turn
-        elif c == curses.KEY_RIGHT:  servo.turn_right(1E-2) # large right turn
-        elif c == curses.KEY_SLEFT:  servo.turn_left(1E-3)  # small left turn
-        elif c == curses.KEY_SRIGHT: servo.turn_right(1E-3) # small right turn
+        elif c == curses.KEY_LEFT:   servo.turn_left(0.05)  # large left turn
+        elif c == curses.KEY_RIGHT:  servo.turn_right(0.05) # large right turn
+        elif c == curses.KEY_SLEFT:  servo.turn_left(0.01)  # small left turn
+        elif c == curses.KEY_SRIGHT: servo.turn_right(0.01) # small right turn
         elif c == curses.KEY_UP:     servo.move_faster()    # moves faster
         elif c == curses.KEY_DOWN:   servo.move_slower()    # slows down
         elif c == ord('0'):          servo.move(0)          # stops the vehicle
-        elif c == ord('1'):          servo.move(0.13)       # slowest speed to move
-        elif c == ord('2'):          servo.move(0.26)       # moderate speed
-        elif c == ord('3'):          servo.move(0.39)       # fastest safe speed
+        elif c == ord('1'):          servo.move(0.14)       # slowest speed to move
+        elif c == ord('2'):          servo.move(0.15)       # moderate speed
+        elif c == ord('3'):          servo.move(0.16)       # fastest safe speed
         elif c == ord('/'):          servo.turn(0)          # sets turn to zero
 
         # Refresh the screen and wait before trying again
