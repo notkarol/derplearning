bfb25857aa0bb9f605a04a6a8eae1e33730fdfad
Karol Zieba <notkarol@gmail.com>
2017-09-29 07:21:34 +0000

    training does not work on tx2

--- train/train_clone.py
+++ train/train_clone.py
@@ -9,45 +9,50 @@ import keras
 from keras.models import Sequential
 from keras.layers import Conv2D, MaxPooling2D, Dropout, Flatten, Dense, Activation
 
-H, W, D, L = 40, 80, 3, 2
+import drputil
+
 
 def model_A(x_in):
-    x = Conv2D(32, (5, 5), activation='elu', padding='same')(x_in)
+    x = Conv2D(32, (3, 3), activation='relu', padding='same')(x_in)
     x = MaxPooling2D(pool_size=(2, 2))(x)
-    x = Conv2D(32, (5, 5), activation='elu', padding='same')(x)
+    x = Conv2D(32, (3, 3), activation='relu', padding='same')(x)
     x = MaxPooling2D(pool_size=(2, 2))(x)
     x = Dropout(0.25)(x)
-    x = Conv2D(32, (5, 5), activation='elu', padding='same')(x)
+    x = Conv2D(32, (3, 3), activation='relu', padding='same')(x)
     x = MaxPooling2D(pool_size=(2, 2))(x)
-    x = Conv2D(32, (3, 3), activation='elu', padding='valid')(x)
+    x = Conv2D(32, (3, 3), activation='relu', padding='same')(x)
+    x = MaxPooling2D(pool_size=(2, 2))(x)
     x = Dropout(0.25)(x)
-    x = Conv2D(32, (3, 3), activation='elu', padding='valid')(x)
     x = Flatten()(x)
-    x_out = Dense(L, name='x_out')(x)
+    x_out = Dense(2, name='x_out')(x)
     return x_out
 
 
-def read_and_decode(filenames):
+def read_and_decode(filenames, config):
 
     # Prepare data readers
     file_queue = tf.train.string_input_producer(filenames, name='queue')
     reader = tf.TFRecordReader()
     key, entire_example = reader.read(file_queue)
-    feature = { 'label': tf.FixedLenFeature([], tf.string),
-                'thumb': tf.FixedLenFeature([], tf.string) }
+    feature = { 'front': tf.FixedLenFeature([], tf.string),
+                'speed': tf.FixedLenFeature([], tf.float32),
+                'steer': tf.FixedLenFeature([], tf.float32) }
     
     features = tf.parse_single_example(entire_example, features=feature)
 
-    # Prepare image
-    label = tf.decode_raw(features['label'], tf.float32)
-    thumb = tf.decode_raw(features['thumb'], tf.uint8)
-    label = tf.reshape(label, [L])
-    thumb = tf.reshape(thumb, [H, W, D])
-    thumb = tf.to_float(thumb)
+    # Prepare label
+    label = tf.stack([features['speed'], features['steer']], 0)
+
+    # Process image
+    camera = 'front'
+    front = tf.decode_raw(features['front'], tf.uint8)
+    front = tf.reshape(front, [config['patch'][camera]['height'],
+                               config['patch'][camera]['width'],
+                               config['patch'][camera]['depth']])
     
     # Return shuffled
     examples, labels = tf.train.shuffle_batch(
-        tensors=[thumb, label],
+        tensors=[front, label],
         batch_size=32,
         num_threads=2,
         enqueue_many=False,
@@ -56,25 +61,31 @@ def read_and_decode(filenames):
     return examples, labels
     
 def main():
-    paths = sys.argv[1:]
+    config = drputil.loadConfig(sys.argv[1])
+    camera = 'front'
+    dataset_path = os.path.join(os.environ["DRP_SCRATCH"], "%s.tfrecords" % config['name'])
+    batch_shape = (32, config['patch'][camera]['height'],
+                   config['patch'][camera]['width'],
+                   config['patch'][camera]['depth'])
     
     with tf.Session() as sess:
-        x_train_batch, y_train_batch = read_and_decode(paths)
+        x_train_batch, y_train_batch = read_and_decode([dataset_path], config)
 
         
         # Prepare variables
         x_train_batch = tf.cast(x_train_batch, tf.float32)
-        x_train_batch = tf.reshape(x_train_batch, shape=(32, H, W, D))
+        x_train_batch = tf.reshape(x_train_batch, shape=batch_shape)
         y_train_batch = tf.cast(y_train_batch, tf.float32)
         x_batch_shape = x_train_batch.get_shape().as_list()
         y_batch_shape = y_train_batch.get_shape().as_list()
+        print(x_batch_shape, y_batch_shape)
         x_train_in = keras.layers.Input(tensor=x_train_batch, batch_shape=x_batch_shape)
         y_train_in = keras.layers.Input(tensor=y_train_batch, batch_shape=y_batch_shape)
         x_train_out = model_A(x_train_in)
                 
         # Prepare model
         model = keras.models.Model(inputs=x_train_in, outputs=x_train_out)
-        model.compile(optimizer='adadelta', loss='mse')
+        model.compile(optimizer='adam', loss='mse')
         model.summary()
         
         # Prepare threads for running data
@@ -82,7 +93,7 @@ def main():
         threads = tf.train.start_queue_runners(coord=coord)
 
         # Train model
-        model.fit(y=y_train_in, epochs=5, steps_per_epoch=5)
+        model.fit(y=y_train_in, epochs=5)
         model.save_weights('weights.h5')        
 
         coord.request_stop()
