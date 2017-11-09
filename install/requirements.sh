3d2fd412586dffa45839d9db687593e1ffad59eb
Karol Zieba <notkarol@gmail.com>
2017-11-09 03:18:19 -0800

    V4l2 installation script

--- install/requirements.sh
+++ install/requirements.sh
@@ -4,16 +4,19 @@
 sudo apt update
 sudo apt upgrade -y
 sudo apt install \
-     libusb-1.0-0-dev \
-     python3-h5py
-sudo apt install python-evdev
+     libusb-1.0-0-dev
 
 # Install Python Packages
 pip3 install --user -U pyusb
+pip3 install --user -U evdev
+
 
 # Make sure we can communicate to Micro Maestro
 sudo cp 99-pololu.rules /etc/udev/rules.d/
 sudo udevadm control --reload-rules
+
+# Install v4l2capture, a python&C library for interfacing with cameras
+bash v4l2capture.sh
 
 # Compile ffmepg from source with GPU support
 bash ffmpeg.sh
