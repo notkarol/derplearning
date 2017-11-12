209b493d8920936c0ddb21349bf6629ba8599140
Karol Zieba <notkarol@gmail.com>
2017-11-11 16:55:41 -0800

    Added bluetooth requirements for managing dualshock directly over bluetooth communication protocol and calculated physical numbers for paras

--- install/requirements.sh
+++ install/requirements.sh
@@ -4,12 +4,13 @@
 sudo apt update
 sudo apt upgrade -y
 sudo apt install \
-     libusb-1.0-0-dev
+     libusb-1.0-0-dev \
+     libbluetooth-dev 
 
 # Install Python Packages
 pip3 install --user -U pyusb
 pip3 install --user -U evdev
-
+pip3 install --user -U pybluez
 
 # Make sure we can communicate to Micro Maestro
 sudo cp 99-pololu.rules /etc/udev/rules.d/
