181ad2a2400bf1b8b7a30012ec8f21d6585973a0
Karol Zieba <notkarol@gmail.com>
2017-11-15 02:48:19 -0800

    Also modified cloen to be nocuda

--- src/derp/scripts/clone.py
+++ src/derp/scripts/clone.py
@@ -2,7 +2,7 @@
 
 import cv2
 import numpy as np
-import os
+from os.path import join
 import sys
 import torch
 from torch.autograd import Variable
@@ -11,11 +11,12 @@ from derp.inferer import Inferer
 
 class Clone(Inferer):
 
-    def __init__(self, hw_config, sw_config, model_dir, state):
+    def __init__(self, hw_config, sw_config, model_dir, state, nocuda=False):
 
         self.hw_config = hw_config
         self.sw_config = sw_config
         self.model_dir = model_dir
+        self.nocuda = nocuda
 
         # Prepare the input camera
         self.component_name = self.sw_config['clone']['patch']['component']
@@ -28,11 +29,11 @@ class Clone(Inferer):
         self.size = (sw_config['clone']['patch']['width'], sw_config['clone']['patch']['height'])
 
         # Prepare model
-        self.model_path = os.path.join(model_dir, 'clone.pt')
+        self.model_path = join(model_dir, 'clone.pt')
         self.model = torch.load(self.model_path)
         self.model.eval()
 
-        
+
     def plan(self, state):
 
         # Prepare input thumbnail
@@ -40,22 +41,26 @@ class Clone(Inferer):
         patch = frame[self.bbox.y : self.bbox.y + self.bbox.h,
                       self.bbox.x : self.bbox.x + self.bbox.w]
         thumb = cv2.resize(patch, self.size, interpolation=cv2.INTER_AREA)
-
+        
         # Prepare batch
         batch = np.reshape(thumb, [1] + list(thumb.shape)).transpose((0, 3, 1, 2))
         batch = torch.from_numpy(batch).float()
-        batch = batch.cuda()
+        if not self.nocuda:
+            batch = batch.cuda()
         batch /= 255
 
         # get predictions
         out = self.model(Variable(batch))
-        predictions = out.data.cpu().numpy()[0]
+        if self.nocuda:
+            predictions = out.data.numpy()[0]
+        else:
+            predictions = out.data.cpu().numpy()[0]
 
         # Store the data we're getting if we're debugging
         if state['record'] and self.sw_config['debug']:
-            cv2.imwrite(os.path.join(state['folder'], "%i_frame.png" % state['timestamp']), frame)
-            cv2.imwrite(os.path.join(state['folder'], "%i_patch.png" % state['timestamp']), patch)
-            cv2.imwrite(os.path.join(state['folder'], "%i_thumb.png" % state['timestamp']), thumb)
+            util.save_image(join(state['folder'], "%i_frame.png" % state['timestamp']), frame)
+            util.save_image(join(state['folder'], "%i_patch.png" % state['timestamp']), patch)
+            util.save_image(join(state['folder'], "%i_thumb.png" % state['timestamp']), thumb)
 
 
         # Desired upodates
