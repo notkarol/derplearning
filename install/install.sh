9295dc29ae74556f9d16375be44b7d2d2d0b84af
Karol Zieba <notkarol@gmail.com>
2017-12-03 13:54:50 +0000

    Install scripts for raspberry pi being tested

--- install/install.sh
+++ install/install.sh
@@ -4,6 +4,7 @@
 sudo apt update
 sudo apt upgrade -y
 sudo apt install \
+     python3-pip \
      libusb-1.0-0-dev \
      libbluetooth-dev \
      libffi-dev \
@@ -21,15 +22,18 @@ sudo cp 99-pololu.rules /etc/udev/rules.d/
 sudo udevadm control --reload-rules
 
 # Add groups we might need to use. netdev for bluetooth and i2c for gpio ports
-sudo usermod -a -G i2c $USER # gpio
-sudo usermod -a -G input $USER # input devices
-sudo usermod -a -G netdev $USER # bluetooth
+if [[ -e $(groups | grep i2c) ]] ; then 
+    sudo usermod -a -G i2c $USER # gpio
+fi
+if [[ -e $(groups | grep input) ]] ; then 
+    sudo usermod -a -G input $USER # input devices
+fi
+if [[ -e $(groups | grep netdev) ]] ; then 
+    sudo usermod -a -G netdev $USER # bluetooth
+fi
 
 # Install v4l2capture, a python&C library for interfacing with cameras
 bash v4l2capture.sh
-
-# Compile OpenCV3 from source
-bash opencv3.sh
 
 # Compile PyTorch from source
 bash pytorch.sh
