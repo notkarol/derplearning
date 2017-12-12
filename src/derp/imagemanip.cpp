fe954ff4df7cc175acce829d0f72c703e0e219dd
john <Johnaellis0@gmail.com>
2017-12-12 00:05:45 -0800

    fleshed out comments on imagemanip.py laid the frame of imagemanip.cpp

---
+++ src/derp/imagemanip.cpp
@@ -0,0 +1,11 @@
+
+
+using namespace std;
+
+
+
+/********Image shifting and rotating ********/
+
+/********Patch selection and cropping *******/
+
+/********Patch Resizing for NN input ********/
--- src/derp/imagemanip.py
+++ src/derp/imagemanip.py
@@ -1,4 +1,5 @@
-import imageio
+#import imageio
+import numpy as np
 
 class Bbox:
     def __init__(self, x, y, w, h):
@@ -21,30 +22,46 @@ def get_patch_bbox(sw_config, hw_config):
 
     width = hw_config['width'] * hfov_ratio
     height = hw_config['height'] * vfov_ratio
-    x = (hw_config['width'] - width) // 2
+    x = (hw_config['width'] - width) // 2 #use floor division("//")
     y = hw_config['height'] - height
+    #y assumes that the crop will always include the lowest row of pixels
 
     return Bbox(x, y, width, height)
 
 
-def perturb(frame, rotate_degrees, shift_meters, config):
-    out = np.zeros(np.shape(img), dtype=np.uint8)
-    horizon_pixels = horizonset(config['height'], config['vfov'],
+'''applies geometric transforms to create the effect of y axis vehical shifting
+and z axis vehicle rotation assuming a flat world'''
+'''this function could be changed to compute and return only the contents of the bbox'''
+def perturb(frame, rotate_degrees, shift_meters, config, margin):
+    out = np.zeros(np.shape(frame), dtype=np.uint8)
+    
+
+    # the row on which the horizon is found
+    horizon_index = horizonset(config['height'], config['vfov'],
                                 config['bottom_row_x'], config['z'])
-    rotate_pixels = config['width'] * rotate_degrees / config['hfov']
+
+    # determines how many pixel shifts will be needed to effect rotation
+    rotate_pixels = rotate_degrees * config['width']  / config['hfov']
+    # determines the max pixel shift required for side shifting
     shift_pixels = ymetertopixel(shift_meters, config['width'], config['hfov'])
 
-    for z, row in enumerate(img):
+    for z, row in enumerate(frame):
         
         # Calculates the shift distance for a given row
         shift_dist = (rotate_pixels +
-                      shift_pixels * (max(0, (z + 1 - horizon_pixels))
-                                      / (len(img) - horizon_pixels)))
+                      shift_pixels * (max(0, (z + 1 - horizon_index))
+                                      / (len(frame) - horizon_index)))
         shift_count = int(round(shift_dist, 0))
 
         # Executes the called for shift accross the row
         if shift_count == 0:
             out[z] = row
+        '''FIXME: modify the loop to only apply to rows needed by the bbox to keep the 
+        margin check from being oversensitive.'''
+        elif np.abs(shift_count) > margin
+            print("""Error shift of %i pixels exceeded margin of %i pixels.
+             This was caused by %f degrees rotation and %f meters of shifting.""",
+            % (shift_count, margin, rotate_degrees, shift_pixels) )
         elif shift_count > 0:
             for y, pixel in enumerate(row):
                 if y + shift_count < len(row):
@@ -61,13 +78,17 @@ def deg2rad(val):
 
 def rad2deg(val):
     return val * 180 / np.pi
-    
-def ymetertopixel(shift, width, hfov, bottom_row_x, z):
-    blah = 2 * np.tan(deg2rad(hfov) / 2) * (z**2 + bottom_row_x**2) ** 0.5
-    return shift * width / blah
-
-def horizonset(height, vfov, bottom_row_x, z):
-    return (height * (vfov - rad2deg(np.arctan(z / bottom_row_x))) / vfov)
 
 
+# Returns the number of pixels which the bottom of the frame needs to be shifted
+# to approximate the car being shifted by the same number of meters(y)
+# bottom_row_x is a measure of the camera's minimum view range (xaxis in meters)
+# hfov is the field of view as measured about the 
+# z is the height of the camera in meters (note camera is at 0,0,0)
+def ymetertopixel(meter_shift, pix_width, hfov, bottom_row_x, z):
+    meter_width = 2 * np.tan(deg2rad(hfov) / 2) * (z**2 + bottom_row_x**2) ** 0.5
+    return meter_shift * pix_width / meter_width
 
+# returns the pixel elevation of the camera's horizon
+def horizonset(pix_height, vfov, bottom_row_x, z):
+    return (pix_height * (vfov -  rad2deg(np.arctan(z / bottom_row_x) ) ) / vfov)
