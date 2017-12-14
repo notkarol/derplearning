e8ca3498608318874359e9c6ac4a4e60df9bda61
Karol Zieba <notkarol@gmail.com>
2017-12-14 02:52:06 -0800

    Pyramid model had a missing import and also increased its size a bit

--- src/derp/models/pyramidmodel.py
+++ src/derp/models/pyramidmodel.py
@@ -1,3 +1,4 @@
+import torch
 import torch.nn as nn
 from derp.models.blocks import ConvBlock, LinearBlock, ViewBlock
 
@@ -6,18 +7,19 @@ class PyramidModel(nn.Module):
     def __init__(self, in_dim, n_status, n_out, verbose=True):
         super(PyramidModel, self).__init__()
         dim = in_dim.copy()
-        self.c1 = ConvBlock(dim, 24, 5, stride=2, verbose=verbose)
-        self.c2 = ConvBlock(dim, 32, 3, pool='max', verbose=verbose)
-        self.c3a = ConvBlock(dim, 40, 3, verbose=verbose)
-        self.c3b = ConvBlock(dim, 48, 3, pool='max', verbose=verbose)
-        self.c4a = ConvBlock(dim, 56, 3, verbose=verbose)
-        self.c4b = ConvBlock(dim, 64, 3, pool='max', verbose=verbose)
-        self.c5a = ConvBlock(dim, 72, 3, verbose=verbose)
-        self.c5b = ConvBlock(dim, 80, 3, pool='max', verbose=verbose)
+        self.c1 = ConvBlock(dim, 32, 5, stride=2, verbose=verbose)
+        self.c2 = ConvBlock(dim, 40, 3, pool='max', verbose=verbose)
+        self.c3a = ConvBlock(dim, 48, 3, verbose=verbose)
+        self.c3b = ConvBlock(dim, 56, 3, pool='max', verbose=verbose)
+        self.c4a = ConvBlock(dim, 64, 3, verbose=verbose)
+        self.c4b = ConvBlock(dim, 72, 3, pool='max', verbose=verbose)
+        self.c5a = ConvBlock(dim, 80, 3, verbose=verbose)
+        self.c5b = ConvBlock(dim, 96, 3, pool='max', verbose=verbose)
         self.view = ViewBlock(dim, verbose=verbose)
         dim[0] += n_status
-        self.fc1 = LinearBlock(dim, 32, verbose=verbose)
-        self.fc2 = LinearBlock(dim, n_out, activation=False, verbose=verbose)
+        self.fc1 = LinearBlock(dim, 64, verbose=verbose)
+        self.fc2 = LinearBlock(dim, 32, verbose=verbose)
+        self.fc3 = LinearBlock(dim, n_out, activation=False, verbose=verbose)
 
     def forward(self, x, status):
         out = self.c1(x)
@@ -32,4 +34,5 @@ class PyramidModel(nn.Module):
         out = torch.cat((out, status), 1)
         out = self.fc1(out)
         out = self.fc2(out)
+        out = self.fc3(out)
         return out
