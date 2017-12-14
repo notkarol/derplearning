36b045eeca48ac0bd5fe5984515e3ec09bc0764d
Karol Zieba <notkarol@gmail.com>
2017-12-14 03:35:30 -0800

    Also upgraded resnet to work well but be wider and deeper for scale's sake

--- src/derp/models/resnet33model.py
+++ src/derp/models/resnet33model.py
@@ -1,4 +1,4 @@
-
+import torch
 import torch.nn as nn
 from derp.models.blocks import *
 
@@ -6,26 +6,28 @@ class Resnet33Model(nn.Module):
     def __init__(self, in_dim, n_status, n_out, verbose=True):
         super(Resnet33Model, self).__init__()
         dim = in_dim.copy()
-        self.c1 = ConvBlock(dim, 32, kernel_size=5, stride=2, verbose=verbose)
-        self.c2a = ResnetBlock(dim, 32, verbose=verbose)
-        self.c2b = ResnetBlock(dim, 32, verbose=verbose)
-        self.c2c = ResnetBlock(dim, 32, pool='max', verbose=verbose)
-        self.c3a = ResnetBlock(dim, 32, verbose=verbose)
-        self.c3b = ResnetBlock(dim, 32, verbose=verbose)
-        self.c3c = ResnetBlock(dim, 32, verbose=verbose)
-        self.c3d = ResnetBlock(dim, 32, verbose=verbose)
-        self.c3e = ResnetBlock(dim, 32, verbose=verbose)
-        self.c3f = ResnetBlock(dim, 32, pool='max', verbose=verbose)
-        self.c4a = ResnetBlock(dim, 32, verbose=verbose)
-        self.c4b = ResnetBlock(dim, 32, verbose=verbose)
-        self.c4c = ResnetBlock(dim, 32, verbose=verbose)
-        self.c4d = ResnetBlock(dim, 32, verbose=verbose)
-        self.c4e = ResnetBlock(dim, 32, verbose=verbose)
-        self.c4f = ResnetBlock(dim, 32, pool='max', verbose=verbose)
+        self.c1 = ConvBlock(dim, 64, kernel_size=5, stride=2, verbose=verbose)
+        self.c2a = ResnetBlock(dim, 64, verbose=verbose)
+        self.c2b = ResnetBlock(dim, 64, verbose=verbose)
+        self.c2c = ResnetBlock(dim, 64, pool='max', verbose=verbose)
+        self.c3a = ResnetBlock(dim, 64, verbose=verbose)
+        self.c3b = ResnetBlock(dim, 64, verbose=verbose)
+        self.c3c = ResnetBlock(dim, 64, verbose=verbose)
+        self.c3d = ResnetBlock(dim, 64, verbose=verbose)
+        self.c3e = ResnetBlock(dim, 64, verbose=verbose)
+        self.c3f = ResnetBlock(dim, 64, pool='max', verbose=verbose)
+        self.c4a = ResnetBlock(dim, 64, verbose=verbose)
+        self.c4b = ResnetBlock(dim, 64, verbose=verbose)
+        self.c4c = ResnetBlock(dim, 64, verbose=verbose)
+        self.c4d = ResnetBlock(dim, 64, verbose=verbose)
+        self.c4e = ResnetBlock(dim, 64, verbose=verbose)
+        self.c4f = ResnetBlock(dim, 64, pool='max', verbose=verbose)
         self.c5 = ConvBlock(dim, 64, kernel_size=3, padding=0, verbose=verbose)
         self.view = ViewBlock(dim, verbose=verbose)
         dim[0] += n_status
-        self.fc1 = LinearBlock(dim, n_out, activation=False, verbose=verbose)
+        self.fc1 = LinearBlock(dim, 64, verbose=verbose)
+        self.fc2 = LinearBlock(dim, 32, verbose=verbose)
+        self.fc3 = LinearBlock(dim, n_out, activation=False, verbose=verbose)
 
     def forward(self, x, status):
         out = self.c1(x)
@@ -45,10 +47,11 @@ class Resnet33Model(nn.Module):
         out = self.c4d(out)
         out = self.c4e(out)
         out = self.c4f(out)
-
         out = self.c5(out)
         out = self.view(out)
         out = torch.cat((out, status), 1)
         out = self.fc1(out)
+        out = self.fc2(out)
+        out = self.fc3(out)
         return out
     
