9e84249dd0199a9c9b44dc12cea0cc94f3778d5b
john <Johnaellis0@gmail.com>
2018-03-23 23:52:53 -0700

    Updated the README and improved documentation of the pipeline script.

--- src/pipeline.sh
+++ src/pipeline.sh
@@ -1,12 +1,24 @@
 #!/bin/bash -x
 
-# SET THESE VARIABLES. car should be an entry in ~/.ssh/config too
-name=$1
-button=$2
-car=${name%%-*}
+'''
+batch move and label collected data
+then create and train a model on this data
+'''
 
-# Copy over training data from car and label it
-rsync -rvP ${car}:/mnt/sdcard/data/* ${DERP_DATA}/train
+# SET THESE VARIABLES. car should be an entry in ~/.ssh/config too
+#name of the vehicle the model will be deployed to
+name=$1
+#which button you press on the DS4 to give the model control of the car
+button=$2
+#created from the name of the car refers to where the model will be deployed to 
+car=${name%%-*}
+#location where the data can be pulled from to the local train folder
+data_source=$3
+
+# Copy over training data from car
+rsync --size-only -rvP $data_source* ${DERP_DATA}/train
+
+# label data in the local train folder if it doesn't have a label file
 for f in $DERP_DATA/train/*
 do
     if ! [[ -e $f/label.csv ]]
@@ -17,14 +29,14 @@ done
 
 # Delete existing trained name and train a new one
 rm -rf ${DERP_SCRATCH}/${name}
-python3 clone_create.py --car $name
-python3 clone_train.py --car $name
+python3 clone_create.py --config $name
+python3 clone_train.py --config $name
 
-# Deploy Model
+# Deploy Model to the target vehicle
 if [[ -n $button ]] ; then
     model=$(ls ${DERP_SCRATCH}/${name}/*pt | tail -n 1)
     rsync -rvP $model ${car}:${DERP_MODEL}/${button}/clone.pt
     if [[ $? --eq "0" ]] ; then
-	echo "SUCCESS"
+	   echo "SUCCESS"
     fi
 fi
