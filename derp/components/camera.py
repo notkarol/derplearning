1481860bb6b01008acb4dc14180a5dca3212207c
Karol Zieba <notkarol@gmail.com>
2018-04-20 09:33:38 +0000

    System does not crash due to camera read failures and recovers from the camera being unplugged and then re-plugged, continuing the stream of data

--- derp/components/camera.py
+++ derp/components/camera.py
@@ -20,8 +20,37 @@ class Camera(Component):
         self.out_buffer = []
         self.frame_counter = 0
         self.start_time = 0
-        
-        # Find available cameras, use most recently plugged in camera
+        self.image_bytes = b''
+        self.state[self.config['name']] = None
+        self.initialize()
+
+    def __del__(self):
+        super(Camera, self).__del__()
+        if self.cap is not None:
+            self.cap.close()
+            self.cap = None
+
+    def initialize(self):
+        if self.cap:
+            del self.cap
+            self.cap = None
+        self.ready = self.find_camera()
+        if not self.ready:
+            return False
+        try:
+            w, h = self.cap.set_format(self.config['width'], self.config['height'], fourcc='MJPG')
+            fps = self.cap.set_fps(self.config['fps'])
+            self.cap.create_buffers(1)
+            self.cap.queue_all_buffers()
+            self.cap.start()
+            self.ready = True
+        except Exception as e:
+            print("Could not initialize capture:", e)
+            self.ready = False
+        return self.ready
+            
+    def find_camera(self):
+        """ Finds and connects to the camera """
         if self.config['index'] is None:
             devices = [int(f[-1]) for f in sorted(os.listdir('/dev'))
                        if re.match(r'^video[0-9]', f)]
@@ -35,54 +64,40 @@ class Camera(Component):
         # Connect to camera, exit if we can't
         try:
             self.cap = v4l2capture.Video_device("/dev/video%i" % self.index)
-        except FileNotFoundError:
-            print("Camera index [%i] not found" % self.index)
-            return
-
-        # start the camerea
-        w, h = self.cap.set_format(self.config['width'], self.config['height'], fourcc='MJPG') # YUYV
-        fps = self.cap.set_fps(self.config['fps'])
-        self.cap.create_buffers(1)
-        self.cap.queue_all_buffers()
-        self.cap.start()
-        self.state[self.config['name']] = np.zeros((h, w, 3), dtype=np.uint8)
-        
-        # Return whether we have succeeded
-        self.ready = True
-
-
-    def __del__(self):
-        super(Camera, self).__del__()
-        if self.cap is not None:
-            self.cap.close()
-            self.cap = None
-
-
-    def sense(self):
-        
-        # Make sure we have a camera open
-        if self.cap is None:
-            return False
-        
-        # Read the next video frame. If we couldn't get it, use the last one
-        frame = None
-        counter = 1
-        while frame is None and counter:
-            counter -= 1
-            select.select((self.cap,), (), ())
+        except:
+            print("Camera index [%i] not found. Trying next." % self.index)
+            self.index += 1
             try:
-                self.state['timestamp'] = time()
-                image_bytes = self.cap.read_and_queue()
-                image_array = np.fromstring(image_bytes, np.uint8)
-                self.state[self.config['name']][:,:,:] = cv2.imdecode(image_array, cv2.IMREAD_COLOR)
-            except Exception as e:
-                print("Camera: Unable to get frame. Retrying")
-                print(e)
-        
-        # Append frame to out buffer if we're writing
-        if self.is_recording():
-            self.out_buffer.append((self.state['timestamp'], image_bytes))
+                self.cap = v4l2capture.Video_device("/dev/video%i" % self.index)
+            except:
+                print("Camera index [%i] not found. Failing." % self.index)
+                self.cap = None
+                return False
         return True
+            
+    def sense(self):
+        """ Read the next video frame. If we couldn't get it, use the previous one """
+        if not self.ready:
+           self.initialize()
+
+        if self.ready:
+            frame = None
+            counter = 1
+            while frame is None and counter:
+                counter -= 1
+                select.select((self.cap,), (), (), 0.1)
+                try:
+                    self.image_bytes = self.cap.read_and_queue()
+                    image_array = np.fromstring(self.image_bytes, np.uint8)
+                    self.state[self.config['name']] = cv2.imdecode(image_array, cv2.IMREAD_COLOR)
+                except Exception as e:
+                    print("Camera: Unable to get frame. Retrying")
+                    self.ready = False
+                    break
+        
+        if self.is_recording():
+            self.out_buffer.append((self.state['timestamp'], self.image_bytes))
+        return self.ready
 
 
     def record(self):
