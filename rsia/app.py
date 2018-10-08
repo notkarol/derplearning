ba928ada4bdee51540251f47f22f08d836bc5941
john <Johnaellis0@gmail.com>
2018-10-07 23:40:37 -0700

    Added the boostrap dashboard template and a route to serve it.

--- rsia/app.py
+++ rsia/app.py
@@ -1,9 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
     return "Hello World!"
+
+@app.route('/dashboard')
+def dashboard():
+    return render_template('dashboard.html')
 
 if __name__ == '__main__':
     app.run()
