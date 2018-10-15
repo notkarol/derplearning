0feb1d79891e8407cf5356e5b1f55fa42782f42f
john <Johnaellis0@gmail.com>
2018-10-14 18:38:35 -0700

    broke out ui into a template with inheritance.

--- rsia/app.py
+++ rsia/app.py
@@ -1,13 +1,16 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-  return "Hello World!"
 
 @app.route('/dashboard_hello_world')
 def dashboard():
   return render_template('dashboard_hello_world.html')
+
+
+@app.route('/data_overlays')
+def graph_display():
+  return render_template('data_overlays.html')
+
 
 if __name__ == '__main__':
   app.run()
