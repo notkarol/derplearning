81fce4f2c64c62bf6d7518f9b1cf3c826c82e2aa
john <Johnaellis0@gmail.com>
2018-10-17 22:54:31 -0700

    moved the chart draw script to the overlay template, copy pasted in a little data from a csv to test.

--- rsia/app.py
+++ rsia/app.py
@@ -9,7 +9,7 @@ def dashboard():
 
 @app.route('/data_overlays')
 def graph_display():
-  return render_template('data_overlays.html')
+  return render_template('data_overlays.html', timeStamps=[505.120, 505.282, 505.417, 505.552], dataSet0=[-0.001093, 0.125391, 0.127344, 0.1625])
 
 
 if __name__ == '__main__':
