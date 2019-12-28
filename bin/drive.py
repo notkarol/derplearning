ab400922d1a24d8951cd3ef57e3ddd5ae806e6cf
Karol Zieba <karol@zieba.dev>
2019-12-28 15:09:36 -0800

    Got drive to end if any subprocesses die

--- bin/drive.py
+++ bin/drive.py
@@ -7,10 +7,17 @@ from pathlib import Path
 import derp.util
 import derp.camera
 import derp.joystick
-import derp.keyboard
 import derp.writer
 from multiprocessing import Process
 import time
+
+
+def all_running(processes):
+    for proc in processes:
+        proc.join(timeout=0)
+        if not proc.is_alive():
+            return False
+    return True
 
 
 def main():
@@ -21,27 +28,18 @@ def main():
 
     config = derp.util.load_config(args.config)
 
-    processes = {}
-    for component_name in sorted(config):
-        if "camera" == component_name:
-            func = derp.camera.run
-        elif "input" == component_name:
-            if config[component_name]['type'] == 'keyboard':
-                func = derp.keyboard.run
-            elif config[component_name]['type'] == 'joystick':
-                func = derp.joystick.run
-            else:
-                continue
-        elif "writer" == component_name:
-            func = derp.writer.run
-        else:
-            continue
-        print("Starting", component_name)
-        processes[component_name] = Process(target=func, args=(config,))
-        processes[component_name].start()
-
-    for component_name in sorted(processes):
-        processes[component_name].join()
+    component_map = {'camera': derp.camera.run,
+                     'joystick': derp.joystick.run,
+                     'writer': derp.writer.run}
+    processes = []
+    for name in sorted(component_map):
+        proc = Process(target=component_map[name], args=(config,))
+        proc.start()
+        processes.append(proc)
+    while all_running(processes):
+        time.sleep(1)
+    for proc in processes:
+        proc.terminate()
 
 
 if __name__ == "__main__":
