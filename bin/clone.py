071695505717039f36b6d4243f171b7857527c4e
Karol Zieba <karol@zieba.dev>
2020-03-06 18:12:39 -0800

    A little more clone and test_end2end cleanup

--- bin/clone.py
+++ bin/clone.py
@@ -16,66 +16,61 @@ import derp.util
 import derp.model
 
 
-def sample_perturbs(config):
-    """ Sample each value of the perurbs we do """
-    sample = {}
-    for perturb in config:
-        sample[perturb] = np.random.uniform(*config[perturb]["range"])
-    return sample
-
-
 def build_recording(args):
     """
     For each frame in the video generate frames and perturbations to save into a dataset.
     """
     config, recording_folder, out_folder = args
-    camera_config = derp.util.load_config(recording_folder / "config.yaml")["camera"]
-    predict_fd = open(str(out_folder / "predict.csv"), "w")
-    status_fd = open(str(out_folder / "status.csv"), "w")
+    camera_config = derp.util.load_config(recording_folder / 'config.yaml')['camera']
+    predict_fd = open(str(out_folder / 'predict.csv'), 'w')
+    status_fd = open(str(out_folder / 'status.csv'), 'w')
 
     topics = derp.util.load_topics(recording_folder)
-    assert "quality" in topics and topics["quality"]
+    assert 'quality' in topics and topics['quality']
 
     actions = derp.util.extract_car_actions(topics)
-    camera = {"times": [msg.publishNS for msg in topics["camera"]]}
-    camera["speed"] = derp.util.extract_latest(camera["times"], actions[:, 0], actions[:, 1])
-    camera["steer"] = derp.util.extract_latest(camera["times"], actions[:, 0], actions[:, 2])
+    camera = {'times': [msg.publishNS for msg in topics['camera']]}
+    camera['speed'] = derp.util.extract_latest(camera['times'], actions[:, 0], actions[:, 1])
+    camera['steer'] = derp.util.extract_latest(camera['times'], actions[:, 0], actions[:, 2])
 
-    bbox = derp.util.get_patch_bbox(config["thumb"], camera_config)
-    size = (config["thumb"]["width"], config["thumb"]["height"])
+    bbox = derp.util.get_patch_bbox(config['thumb'], camera_config)
+    size = (config['thumb']['width'], config['thumb']['height'])
     n_frames_processed = 0
-    for camera_i, timestamp in enumerate(camera["times"]):
-        if topics["quality"][camera_i].quality != "good":
+    for camera_i, timestamp in enumerate(camera['times']):
+        if topics['quality'][camera_i].quality != 'good':
             continue
-        # Skip the first/last 2 seconds of video no matter how it's labeled
-        if timestamp < camera["times"][0] + 2e6 or timestamp > camera["times"][-1] - 2e6:
-            continue
-        for perturb_i in range(config["build"]["n_samples"]):
-            store_name = "%i_%03i.png" % (timestamp, perturb_i)
-            perturbs = sample_perturbs(config["build"]["perturbs"])
+        for perturb_i in range(config['build']['n_samples']):
+            store_name = '%i_%03i.png' % (timestamp, perturb_i)
 
-            predict = [store_name]
-            for predictor_config in config["predict"]:
-                predictor_timestamp = int(timestamp + predictor_config["time_offset"] * 1e6)
-                index = np.searchsorted(camera["times"], predictor_timestamp)
-                predict.append(float(camera[predictor_config["field"]][index]))
-
-            status = [store_name]
-            for status_config in config["status"]:
-                status.append(float(camera[status_config["field"]][camera["index"] - 1]))
-
-            frame = derp.util.decode_jpg(topics["camera"][camera_i].jpg)
-            derp.util.perturb(frame, camera_config, perturbs)
+            # Prepare image with Perturbations
+            shift = np.random.uniform(*config['build']['perturbs']['shift']['range'])
+            rotate = np.random.uniform(*config['build']['perturbs']['rotate']['range'])
+            frame = derp.util.decode_jpg(topics['camera'][camera_i].jpg)
+            derp.util.perturb(frame, camera_config, shift, rotate)
             patch = derp.util.crop(frame, bbox)
             thumb = derp.util.resize(patch, size)
             derp.util.save_image(out_folder / store_name, thumb)
 
-            predict_row = ["%.6f" % x if isinstance(x, float) else x for x in predict]
-            predict_fd.write(",".join(predict_row) + "\n")
-            status_row = ["%.6f" % x if isinstance(x, float) else x for x in status]
-            status_fd.write(",".join(status_row) + "\n")
+            predict = [store_name]
+            for predictor_config in config['predict']:
+                predictor_timestamp = int(timestamp + predictor_config['time_offset'] * 1e6)
+                index = np.searchsorted(camera['times'], predictor_timestamp)
+                value = float(camera[predictor_config['field']][index])
+                if predictor_config['field'] == 'steer':
+                    value += config['build']['perturbs']['shift']['fudge'] * shift
+                    value += config['build']['perturbs']['rotate']['fudge'] * rotate
+                predict.append(value)
+
+            status = [store_name]
+            for status_config in config['status']:
+                status.append(float(camera[status_config['field']][camera['index']]))
+
+            predict_row = ['%.6f' % x if isinstance(x, float) else x for x in predict]
+            predict_fd.write(','.join(predict_row) + '\n')
+            status_row = ['%.6f' % x if isinstance(x, float) else x for x in status]
+            status_fd.write(','.join(status_row) + '\n')
         n_frames_processed += 1
-    print("Build %s %5i %s" % (recording_folder, n_frames_processed, out_folder))
+    print('Build %s %5i %s' % (recording_folder, n_frames_processed, out_folder))
     predict_fd.close()
     status_fd.close()
     return True
@@ -83,10 +78,10 @@ def build_recording(args):
 
 def build(config, experiment_path, count):
     """ Build the dataset """
-    np.random.seed(config["seed"])
+    np.random.seed(config['seed'])
     process_args = []
-    for recording_folder in derp.util.RECORDING_ROOT.glob("recording-*-*-*"):
-        partition = "train" if np.random.rand() < config["build"]["train_chance"] else "test"
+    for recording_folder in derp.util.RECORDING_ROOT.glob('recording-*-*-*'):
+        partition = 'train' if np.random.rand() < config['build']['train_chance'] else 'test'
         out_folder = experiment_path / partition / recording_folder.stem
         if not out_folder.exists():
             out_folder.mkdir(parents=True)
@@ -97,46 +92,46 @@ def build(config, experiment_path, count):
 
 
 def train(config, experiment_path, gpu):
-    device = torch.device("cuda:" + gpu if torch.cuda.is_available() else "cpu")
-    model_fn = eval("derp.model." + config["train"]["model"])
-    criterion = eval("torch.nn." + config["train"]["criterion"])().to(device)
-    optimizer_fn = eval("torch.optim." + config["train"]["optimizer"])
+    device = torch.device('cuda:' + gpu if torch.cuda.is_available() else 'cpu')
+    model_fn = eval('derp.model.' + config['train']['model'])
+    criterion = eval('torch.nn.' + config['train']['criterion'])().to(device)
+    optimizer_fn = eval('torch.optim.' + config['train']['optimizer'])
     scheduler_fn = torch.optim.lr_scheduler.ReduceLROnPlateau
-    dim_in = np.array([config["thumb"][x] for x in ["depth", "height", "width"]])
+    dim_in = np.array([config['thumb'][x] for x in ['depth', 'height', 'width']])
 
     # Prepare transforms
-    transformer = derp.model.compose_transforms(config["train"]["transforms"])
-    train_fetcher = Fetcher(experiment_path / "train", transformer, config["predict"])
+    transformer = derp.model.compose_transforms(config['train']['transforms'])
+    train_fetcher = Fetcher(experiment_path / 'train', transformer, config['predict'])
     assert len(train_fetcher)
-    test_fetcher = Fetcher(experiment_path / "test", transformer, config["predict"])
+    test_fetcher = Fetcher(experiment_path / 'test', transformer, config['predict'])
     assert len(test_fetcher)
     train_loader = DataLoader(
-        train_fetcher, config["train"]["batch_size"], shuffle=True, num_workers=3,
+        train_fetcher, config['train']['batch_size'], shuffle=True, num_workers=3,
     )
-    test_loader = DataLoader(test_fetcher, config["train"]["batch_size"], num_workers=3)
-    print("Train Loader: %6i" % len(train_loader.dataset))
-    print("Test  Loader: %6i" % len(test_loader.dataset))
-    np.random.seed(config["seed"])
-    torch.manual_seed(config["seed"])
-    n_status = len(config["status"])
-    n_predict = len(config["predict"])
+    test_loader = DataLoader(test_fetcher, config['train']['batch_size'], num_workers=3)
+    print('Train Loader: %6i' % len(train_loader.dataset))
+    print('Test  Loader: %6i' % len(test_loader.dataset))
+    np.random.seed(config['seed'])
+    torch.manual_seed(config['seed'])
+    n_status = len(config['status'])
+    n_predict = len(config['predict'])
     model = model_fn(dim_in, n_status, n_predict).to(device)
-    optimizer = optimizer_fn(model.parameters(), config["train"]["learning_rate"])
+    optimizer = optimizer_fn(model.parameters(), config['train']['learning_rate'])
     scheduler = scheduler_fn(optimizer, factor=0.25, verbose=True, patience=8)
     loss_threshold = derp.model.test_epoch(device, model, optimizer, criterion, test_loader)
-    print("initial loss: %.6f" % loss_threshold)
-    for epoch in range(config["train"]["epochs"]):
+    print('initial loss: %.6f' % loss_threshold)
+    for epoch in range(config['train']['epochs']):
         start_time = time.time()
         train_loss = derp.model.train_epoch(device, model, optimizer, criterion, train_loader)
         test_loss = derp.model.test_epoch(device, model, criterion, test_loader)
         scheduler.step(test_loss)
-        note = ""
+        note = ''
         if test_loss < loss_threshold:
             loss_threshold = test_loss
-            torch.save(model, str(experiment_path / "model.pt"))
-            note = "saved"
+            torch.save(model, str(experiment_path / 'model.pt'))
+            note = 'saved'
         duration = time.time() - start_time
-        print("Epoch %5i %.6f %.6f %.1fs %s" % (epoch, train_loss, test_loss, duration, note))
+        print('Epoch %5i %.6f %.6f %.1fs %s' % (epoch, train_loss, test_loss, duration, note))
 
 
 def main():
@@ -145,14 +140,14 @@ def main():
     the controller dataset folder every time the validation loss decreases.
     """
     parser = argparse.ArgumentParser()
-    parser.add_argument("brain", type=Path, help="Controller we wish to train")
-    parser.add_argument("--gpu", type=str, default="0", help="GPU to use")
-    parser.add_argument("--count", type=int, default=4, help="parallel processes to build with")
+    parser.add_argument('brain', type=Path, help='Controller we wish to train')
+    parser.add_argument('--gpu', type=str, default='0', help='GPU to use')
+    parser.add_argument('--count', type=int, default=4, help='parallel processes to build with')
 
     args = parser.parse_args()
 
     config = derp.util.load_config(args.brain)
-    experiment_path = derp.util.MODEL_ROOT / config["name"]
+    experiment_path = derp.util.MODEL_ROOT / config['name']
     experiment_path.mkdir(parents=True, exist_ok=True)
 
     build(config, experiment_path, args.count)
--- test/test_end2end.py
+++ test/test_end2end.py
@@ -6,9 +6,21 @@ import torchvision.transforms as transforms
 import derp.util
 import derp.model
 
+
 @pytest.fixture
 def frame():
     return derp.util.load_image("test/100deg.jpg")
+
+
+@pytest.fixture
+def source_config():
+    return {'hfov': 50, 'vfov': 50, 'yaw': 0, 'pitch': 0, 'width': 100, 'height': 100,
+            'x': 0, 'y': 0, 'z': 1}
+
+
+@pytest.fixture
+def target_config():
+    return {'hfov': 32, 'vfov': 32, 'yaw': 0, 'pitch': -4, 'x': 0, 'y': 0, 'z': 1}
 
 
 class Fetcher(torch.utils.data.Dataset):
@@ -22,42 +34,33 @@ class Fetcher(torch.utils.data.Dataset):
         return len(self.table)
 
 
-def test_perturb_learnability(frame):
-    """ verify that the same pixels of all images are cropped as we change yaw """
-    source_config = {'hfov': 50, 'vfov': 50, 'yaw': 0, 'pitch': 0, 'width': 100, 'height': 100,
-                     'x': 0, 'y': 0, 'z': 1}
-    target_config = {'hfov': 32, 'vfov': 24, 'yaw': 0, 'pitch': -4, 'x': 0, 'y': 0, 'z': 1}
+def test_perturb(frame, source_config, target_config):
+    """ verify that a zero perturb does nothing to the pixels """
     bbox = derp.util.get_patch_bbox(target_config, source_config)
     zero_frame = derp.util.perturb(frame.copy(), source_config)
     assert (zero_frame - frame).sum() == 0
 
-    train_table = []
-    test_table = []
+
+def test_perturb_learnability(frame, source_config, target_config):
+    bbox = derp.util.get_patch_bbox(target_config, source_config)
+    train_table, test_table = [], []
     for shift in np.linspace(-0.4, 0.4, 51):
         for rotate in np.linspace(-4, 4, 51):
             p_frame = derp.util.perturb(frame.copy(), source_config, shift, rotate)
             p_patch = derp.util.crop(p_frame, bbox)
             table = test_table if shift == 0 or rotate == 0 else train_table
             table.append([p_patch, torch.FloatTensor(), torch.FloatTensor([shift * 2.5,
-                                                                           rotate / 4])])
+                                                                           rotate * 0.25])])
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu") 
-    batch_size = 32
-    learning_rate = 1E-4
-    n_epochs = 8
-    train_fetcher = Fetcher(train_table)
-    test_fetcher = Fetcher(test_table)
-    train_loader = torch.utils.data.DataLoader(train_fetcher, batch_size, shuffle=True)
-    test_loader = torch.utils.data.DataLoader(test_fetcher, batch_size)
-    model = derp.model.Tiny(np.array([3, 48, 64]), 0, 2).to(device)
-    optimizer = torch.optim.AdamW(model.parameters(), learning_rate)
+    train_fetcher, test_fetcher = Fetcher(train_table), Fetcher(test_table)
+    train_loader = torch.utils.data.DataLoader(train_fetcher, 32, shuffle=True)
+    test_loader = torch.utils.data.DataLoader(test_fetcher, len(test_fetcher))
+    model = derp.model.Tiny(np.roll(train_table[0][0].shape, 1), 0, 2).to(device)
+    optimizer = torch.optim.AdamW(model.parameters(), 1E-3)
     criterion = torch.nn.MSELoss().to(device)
     test_losses = []
-    for epoch in range(n_epochs):
+    for epoch in range(5):
         train_loss = derp.model.train_epoch(device, model, optimizer, criterion, train_loader)
         test_loss = derp.model.test_epoch(device, model, criterion, test_loader)
         test_losses.append(test_loss)
-    print(test_losses)
-    assert min(test_losses) < 1E-2
-    
-            
-        
+    assert min(test_losses) < 2E-3
