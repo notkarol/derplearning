8f97b0dada6773f768d00447e60f6d02af99ac80
Karol Zieba <karol@zieba.dev>
2020-03-16 22:09:01 -0700

    Brains now can initialize pub/sub seperately from super constructor so there isn't a long lag while the model code re-processes a huge backlog of camera input

--- derp/brain.py
+++ derp/brain.py
@@ -12,10 +12,10 @@ class Brain(Part):
     The root class of any object that manipulate's the car state based on some heuristic.
     """
 
-    def __init__(self, config):
+    def __init__(self, config, init_pubsub=True):
         """Preset some common constructor parameters"""
-        super(Brain, self).__init__(config, "brain", ["camera", "joystick", "imu"])
-        self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+        super(Brain, self).__init__(config, 'brain', ['camera', 'joystick', 'imu'], init_pubsub)
+        self.device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
         self.speed = 0
         self.steer = 0
 
@@ -25,9 +25,9 @@ class Brain(Part):
     def run(self):
         """ Publish an action at every camera timestamp """
         topic = self.subscribe()
-        if topic == "camera" and self.predict():
-            self.publish("action", isManual=False, speed=float(self.speed), steer=float(self.steer))
-        if topic == "controller" and self._messages[topic].exit:
+        if topic == 'camera' and self.predict():
+            self.publish('action', isManual=False, speed=float(self.speed), steer=float(self.steer))
+        if topic == 'controller' and self._messages[topic].exit:
             return False
         return True
 
@@ -51,16 +51,19 @@ class Brain(Part):
 
 class Clone(Brain):
     def __init__(self, config):
-        super(Clone, self).__init__(config)
-        model_path = derp.util.MODEL_ROOT / self._config["name"] / "model.pt"
+        super(Clone, self).__init__(config, False)
+        model_path = derp.util.MODEL_ROOT / self._config['name'] / 'model.pt'
         self.model = torch.load(model_path).to(self.device) if model_path.exists() else None
-        self.bbox = derp.util.get_patch_bbox(self._config["thumb"], self._global_config["camera"])
-        self.size = (self._config["thumb"]["width"], self._config["thumb"]["height"])
+        self.bbox = derp.util.get_patch_bbox(self._config['thumb'], self._global_config['camera'])
+        self.size = (self._config['thumb']['width'], self._config['thumb']['height'])
+        self.init_pubsub()
 
     def predict(self):
         if self.model is None:
             return False
-        frame = derp.util.decode_jpg(self._messages["camera"].jpg)
+        if self.bbox is None:
+            return False
+        frame = derp.util.decode_jpg(self._messages['camera'].jpg)
         patch = derp.util.crop(frame, self.bbox)
         thumb = derp.util.resize(patch, self.size) / 255
         status_batch = self.batch_vector([])
@@ -69,12 +72,12 @@ class Clone(Brain):
         prediction_batch = self.model(thumb_batch, status_batch)
         predictions = self.unbatch(prediction_batch)[0]
 
-        self.speed = self._messages["controller"].speedOffset
-        for prediction, config in zip(predictions, self._config["predict"]):
-            if config["name"] == "steer":
+        self.speed = self._messages['controller'].speedOffset
+        for prediction, config in zip(predictions, self._config['predict']):
+            if config['name'] == 'steer':
                 self.steer = float(prediction)
-            elif config["name"] == "speed":
+            elif config['name'] == 'speed':
                 self.speed = float(prediction)
-            elif config["name"] == "future_steer":
+            elif config['name'] == 'future_steer':
                 self.speed *= 1 + (1 - abs(float(prediction)))
         return True
--- derp/part.py
+++ derp/part.py
@@ -6,25 +6,39 @@ from derp.util import TOPICS, MSG_STEM, init_logger, subscriber, publisher, get_
 class Part:
     """ The root class for every part, includes a bunch of useful functions and cleanup """
 
-    def __init__(self, config, name, sub_names):
+    def __init__(self, config, name, sub_names, init_pubsub=True):
         """ By default every part is its own publisher and subscribes to one/many messages """
         self._name = name
+        self._sub_names = sub_names
         self._config = config[name]
         self._global_config = config
         self._logger = init_logger(name, config['recording_path'])
         self._logger.info("__init__")
         self._messages = {topic: TOPICS[topic].new_message() for topic in TOPICS}
-        self._sub_context, self._subscriber = subscriber([MSG_STEM + name for name in sub_names])
-        self._pub_context, self._publisher = publisher(MSG_STEM + name)
+        self._sub_context, self._subscriber = None, None
+        self._pub_context, self._publisher = None, None
+        self._is_pubsub_initialized = False
         self._timestamp = 0
+        if init_pubsub:
+            self.init_pubsub()
 
     def __del__(self):
         """ Clean up the pub/sub system """
         self._logger.info("__del__")
-        self._subscriber.close()
-        self._sub_context.term()
-        self._publisher.close()
-        self._pub_context.term()
+        if self._subscriber:
+            self._subscriber.close()
+        if self._sub_context:
+            self._sub_context.term()
+        if self._publisher:
+            self._publisher.close()
+        if self._pub_context:
+            self._pub_context.term()
+
+    def init_pubsub(self):
+        sub_paths = [MSG_STEM + name for name in self._sub_names]
+        self._sub_context, self._subscriber = subscriber(sub_paths)
+        self._pub_context, self._publisher = publisher(MSG_STEM + self._name)
+        self._is_pubsub_initialized = True
 
     def __repr__(self):
         return self.__class__.__name__.lower()
@@ -36,16 +50,19 @@ class Part:
         assert True
 
     def subscribe(self):
+        if not self._is_pubsub_initialized:
+            return None
         topic_bytes, message_bytes = self._subscriber.recv_multipart()
         self._timestamp = get_timestamp()
         topic = topic_bytes.decode()
-        if topic == "":
-            return None
         self._messages[topic] = TOPICS[topic].from_bytes(message_bytes).as_builder()
         return topic
 
     def publish(self, topic, **kwargs):
+        if not self._is_pubsub_initialized:
+            return None
         message = TOPICS[topic].new_message(
             createNS=self._timestamp, publishNS=get_timestamp(), **kwargs
         )
         self._publisher.send_multipart([str.encode(topic), message.to_bytes()])
+        return message
